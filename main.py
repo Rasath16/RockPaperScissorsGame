864f443baff88df657cdfb59822fd360f731f960
Rasath16 <tharusharasath@gmail.com>
2024-01-15 10:33:54 +0000

    invalid input update

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ while True:
     print(paper)
   if choice == 3:
     print(scissors)
+  else:
+    print("Please enter a valid number")
   print ("Computer Chose :")
   random_number= random.randint(1,3)
   if random_number == 1:
